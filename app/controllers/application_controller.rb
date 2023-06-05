67e55bfb40ef4fd2a11348636062accf300156f3
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-06-05 08:48:49 +0300

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+     # get all the bakeries from the database
+     bakeries = Bakery.all
+     # send them back as a JSON array
+     bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(only: [:name], include: {baked_goods: {only: [:name, :price]}})
+  end
+
+  get '/baked_goods/by_price' do 
+    baked_goods = BakedGood.all.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do 
+    most_expensive = BakedGood.all.order(price: :desc)
+    most_expensive.first.to_json
+  end
 
 end
